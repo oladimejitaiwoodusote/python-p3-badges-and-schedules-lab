f32e069346195660f9a0d0d77716bb1011fc0f3e
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-04-03 13:32:48 -0400

    python lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,32 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    a = []
+    for i in names:
+        a.append(f"Hello, my name is {i}.")
+
+    return a
 
 def assign_rooms(names):
-    return None
+    a = []
+    sum = 1
+    for i in names:
+        a.append(f"Hello, {i}! You'll be assigned to room {sum}!")
+        sum += 1
+
+    return a
 
 def printer(names):
-    return None
+    #print(batch_badge_creator(names))
+    #print(assign_rooms(names))
+    for i in batch_badge_creator(names):
+        print(i)
+    
+    for i in assign_rooms(names):
+        print(i)
+
+
+
+
+printer(["Arel", "Carol"])
